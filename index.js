4a5eff0b22888bd61d0e6332a8d9a49eb9c73537
Priyam Sarma <priyam.sarma@gmail.com>
2017-09-22 15:20:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ function addToCart(item) {
 function viewCart() {
   if (cart.length === 0) {
     console.log("Your shopping cart is empty.")
-  }else if (cart.length ===1) {
+  }/*else if (cart.length ===1) {
     const price = cart[0][Object.keys(cart[0])]
     console.log(`In your cart, you have ${Object.keys(cart[0])} at $${price}.`)
-  }else if (cart.length ===2) {
+  }*/else if (cart.length ===2) {
     const price = cart[0][Object.keys(cart[0])]
     const price1 = cart[1][Object.keys(cart[1])]
     console.log(`In your cart, you have ${Object.keys(cart[0])} at $${price} and ${Object.keys(cart[1])} at $${price1}.`)
